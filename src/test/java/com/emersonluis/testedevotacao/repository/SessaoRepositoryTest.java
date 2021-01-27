55707facf26fe21d118f3390838683e381a13783
Emerson Luis P <54823293+emersoneton@users.noreply.github.com>
2021-01-27 20:40:04 -0300

    Realizado testes na Classe Repository da Sessão (Busca de ID por Respostas)

---
+++ src/test/java/com/emersonluis/testedevotacao/repository/SessaoRepositoryTest.java
@@ -0,0 +1,37 @@
+package com.emersonluis.testedevotacao.repository;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.emersonluis.testedevotacao.datasourse.model.Sessao;
+
+import lombok.Data;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Data
+public class SessaoRepositoryTest {
+
+	@Autowired
+	private SessaoRepository repository;
+	
+	@Test
+	public void TestarCadastroDeSessaoConformeRespostaForSim() {
+		Optional<Sessao> sessao = repository.findById((long) 1);
+		assertThat(sessao.get().getResposta().equals("Sim"));
+	}
+	
+	@Test
+	public void TestarCadastroDeSessaoConformeRespostaForNao() {
+		Optional<Sessao> sessao = repository.findById((long) 2);
+		assertThat(sessao.get().getResposta().equals("Nao"));
+	}
+	
+}
